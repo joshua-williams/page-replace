ecf06f8cc72d45ce4f61b6b5ce5e6b0778a238c5
= <=>
2015-11-07 18:08:16 -0600

    remove extra spaces and semi-colon

--- replace-page.js
+++ replace-page.js
@@ -6,8 +6,8 @@ function replacePage(url){
         meta.setAttribute('name','viewport');
         meta.setAttribute('content', 'width=device-width, initial-scale=1');
         document.getElementsByTagName('head')[0].appendChild(meta);
-        var html = '<style>body{border:0px;padding:0px;margin:0px;background:none;;overflow:hidden;}' +
-            'iframe{height: 100vh;width: 100vw;border:0px;margin:0px;padding: 0px;</style>' +
+        var html = '<style>body{border:0px;padding:0px;margin:0px;background:none;overflow:hidden;}' +
+            'iframe{height:100vh;width:100vw;border:0px;margin:0px;padding:0px;}</style>' +
             '<iframe src="'+window.replacePageUrl+'"></iframe>';
         body.innerHTML = html;
     });
